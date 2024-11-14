6ba981eb4312416385c933b805b0e660397428c6
MixFixLixx <129180952+MixFixLix@users.noreply.github.com>
2024-11-14 20:47:15 +0300

    Bug fixing *( JS )

--- js scripts/mobile_version.js
+++ js scripts/mobile_version.js
@@ -3,6 +3,9 @@ let Modal = document.querySelector(".list");
 let ModalNav = document.querySelector(".nav__menu");
 let ModalIs = false;
 let head = document.querySelector('header');
+let navWord1 = document.querySelector('.nav__word1');
+let navWord2 = document.querySelector('.nav__word2');
+let navWord3 = document.querySelector('.nav__word3');
 
 function Open() {
     Modal.style.display = "block";
@@ -19,6 +22,9 @@ function ModalWindow() {
         Modal.style.opacity = 0;
         setTimeout(function() { 
             Modal.style.display = "none";
+            navWord1.style.display = "none";
+            navWord2.style.display = "none";
+            navWord3.style.display = "none";
     }, 200);
         ModalIs = false;
         btnOpenModal.classList.remove('open');
@@ -28,11 +34,15 @@ function ModalWindow() {
         Modal.style.transition = '.3s';
         Modal.style.opacity = 1;
         Modal.style.display = "block";
-    //     setTimeout(function() { 
-    //         Modal.style.display = "block";
-    // }, 200);
+        navWord1.style.display = "block";
+        navWord2.style.display = "block";
+        navWord3.style.display = "block";
+
         ModalIs = true;
         btnOpenModal.classList.add('open');
         head.style.zIndex = 5000000;
+
+
+        
     }
 }
