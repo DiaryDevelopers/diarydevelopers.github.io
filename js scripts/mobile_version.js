90d76ab4ad595843ab71719f5ea34b6e24bab64d
MixFixLixx <129180952+MixFixLix@users.noreply.github.com>
2024-11-15 21:58:04 +0300

    Update mobile_version.js

--- js scripts/mobile_version.js
+++ js scripts/mobile_version.js
@@ -3,9 +3,6 @@ let Modal = document.querySelector(".list");
 let ModalNav = document.querySelector(".nav__menu");
 let ModalIs = false;
 let head = document.querySelector('header');
-let navWord1 = document.querySelector('.nav__word1');
-let navWord2 = document.querySelector('.nav__word2');
-let navWord3 = document.querySelector('.nav__word3');
 
 function Open() {
     Modal.style.display = "block";
@@ -22,9 +19,6 @@ function ModalWindow() {
         Modal.style.opacity = 0;
         setTimeout(function() { 
             Modal.style.display = "none";
-            navWord1.style.display = "none";
-            navWord2.style.display = "none";
-            navWord3.style.display = "none";
     }, 200);
         ModalIs = false;
         btnOpenModal.classList.remove('open');
@@ -34,9 +28,6 @@ function ModalWindow() {
         Modal.style.transition = '.3s';
         Modal.style.opacity = 1;
         Modal.style.display = "block";
-        navWord1.style.display = "block";
-        navWord2.style.display = "block";
-        navWord3.style.display = "block";
 
         ModalIs = true;
         btnOpenModal.classList.add('open');
