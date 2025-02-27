4b825303dffa24828a84824c2e645e7355b40f44
YarikProgs <z.yaroslav2009@gmail.com>
2025-02-27 22:39:32 +0300

    feat: logging with server

--- sgo/login/scipts/auth.js
+++ sgo/login/scipts/auth.js
@@ -2,13 +2,33 @@ const authButton = document.querySelector(".authButton")
 const inputFieldLogin = document.querySelector("#loginField");
 const inputFieldPassword = document.querySelector("#passswordField");
 
-authButton.onclick = () => {
+authButton.onclick = async () => {
     if (checkIfEmpty(inputFieldLogin) || checkIfEmpty(inputFieldPassword)) return;
 
     const login = inputFieldLogin.value;
     const password = inputFieldPassword.value;
 
-    // TODO DO SOMETHING USEFUL
+    // TODO replace with actual ip
+    const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            login, password
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).catch(() => {})
+
+    if (!response.ok) {
+        document.querySelector(".undefined").style.opacity = '1'
+        const {message} = await response.json();
+        console.error(`Failed to login: ${message}`)
+        return
+    }
+
+    console.log("Successfully logged in");
+    const {token, message} = await response.json();
+    // todo save token in cookies
 
     document.querySelector(".undefined").style.opacity = "1";
 }
